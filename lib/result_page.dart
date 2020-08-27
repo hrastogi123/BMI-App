5f606436faf2fc4d0a16e0521a040c61edb7cfd8
Himanshu Raj <hrastogi1998@gmail.com>
2020-08-27 16:14:59 +0530

    add UI of ResultPage

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,11 +1,56 @@
+import 'package:bmi_app/constant.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'resuable_card.dart';
 
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: Text('BMI CALCULATOR'),
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+                padding: EdgeInsets.only(left: 15),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                'Your Result',
+                style: numberTextStyle,
+            ),
+              )),
+          Expanded(
+            flex: 5,
+            child: ReuseableCard(
+              colour: Color(0xFF1D1E33),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Text(
+                    '18.5',
+                    style: TextStyle(
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('Your BMI is Low, Eat More !', style: lableTextStyle),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
